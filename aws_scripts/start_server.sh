ad66adb0a7c065583e013e0f6d93aa1436b7b791
Vivek Maithani <vivekmaithani@gmail.com>
2024-01-10 23:52:42 -0500

    Run script in bg and kill the server if it's already running

--- aws_scripts/start_server.sh
+++ aws_scripts/start_server.sh
@@ -1,3 +1,10 @@
+# This script is used to start the server on the EC2 instance
+
+# Pull the latest code from the repo
 git pull
+# Build project
 npm run build
-npm run start
+# Kill the server if it is running
+kill -9 $(lsof -t -i:3000)
+# Start the server
+nohup npm run start &
